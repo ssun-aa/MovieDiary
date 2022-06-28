21daa96406f000a1a7b7d81ec2d70be54a4c8fe8
ssun-aa <ssk6200@daum.net>
2022-06-28 22:37:49 +0900

    style: layout css

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,12 +1,16 @@
 import { Outlet } from 'react-router-dom';
+import Navigation from 'components/Navigation';
+import React from 'react';
 import styles from './layout.module.scss';
 
 function Layout() {
   return (
-    <div className={styles.app}>
+    <div className={styles.layout}>
       <div className={styles.wrap}>
-        <Outlet />
-        <nav className={styles.nav} />
+        <Navigation />
+        <React.Suspense fallback={<div>Loading</div>}>
+          <Outlet />
+        </React.Suspense>
       </div>
     </div>
   );
